f51b5d245213e64f2857c4787972cc21c8676864
pilingjurkaanda <aandapilingjurka@gmail.com>
2024-06-04 13:54:50 -0700

    Slider tek Home Page

--- home.php
+++ home.php
@@ -2,15 +2,8 @@
 include "include/header.php";
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beauty And Makeup</title>
+<div class="slide-bar">
+        <div class="img" id="image-slider">
 
-</head>
-<body>
- 
-</body>
-</html>
+        </div>
+</div>
