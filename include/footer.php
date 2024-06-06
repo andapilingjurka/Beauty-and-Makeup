e5402c3d107b17c53b66b537d70961ea0dd130a5
pilingjurkaanda <aandapilingjurka@gmail.com>
2024-06-05 22:50:05 -0700

    Krijimi i Footer

---
+++ include/footer.php
@@ -0,0 +1,41 @@
+<div class="main" data-aos="fade-up"
+     data-aos-easing="linear"
+     data-aos-duration="500">
+        <footer>
+            <div class="waves">
+                <div class="wave" id="wave1"></div>
+            </div>
+
+            <h2>Aquamarine</h2>
+
+            <ul class="menu">
+                <li><a href="./home.php">Home</a></li>
+                <li><a href="./aboutus.php">About Us</a></li>
+                <li><a href="#">Make Up</a></li>
+                <li><a href="#">Skin Care</a></li>
+                <li><a href="#">How To</a></li>
+            </ul>
+
+            <div class="zoom">
+                <ul class="social_icon">
+                    <li><a href="#"><ion-icon name="logo-facebook"></ion-icon>
+                        </a></li>
+
+                    <li><a href="#"><ion-icon name="logo-tiktok"></ion-icon>
+                        </a></li>
+
+                    <li><a href="#"><ion-icon name="logo-instagram"></ion-icon>
+                        </a></li>
+
+                    <li><a href="#"><ion-icon name="logo-pinterest"></ion-icon>
+                        </a></li>
+
+                </ul>
+
+            </div>
+
+            <p>© Aquamarine | 2024</p>
+        </footer>
+    </div>
+</body>
+</html>
