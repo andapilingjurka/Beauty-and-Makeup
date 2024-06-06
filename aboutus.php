abb67a58d21016cdcd194fe5c69497d6b4dbfa95
pilingjurkaanda <aandapilingjurka@gmail.com>
2024-06-05 22:51:40 -0700

    Krijimi i Page AboutUs

---
+++ aboutus.php
@@ -0,0 +1,66 @@
+<?php
+include "include/header.php";
+?>
+
+<div class="container section-aboutus">
+    <div class="slider-aboutus" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000">
+        <div class="wrapper aboutus-wrapper">
+            <div class="img-area">
+                <img src="images/cub1.jpg">
+                <img src="images/cub3.jpg"> 
+                <img src="images/cub2.jpg"> 
+                <img src="images/cub4.jpg">
+            </div>
+        </div>
+    </div>
+    <div class="text-content" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000">
+        <h1>About Us</h1>
+        <p>Aquamarine is your premier destination for indulging 
+            in the world of beauty. With an exquisite array of 
+            makeup, skincare, and perfumes, we aim to redefine 
+            your beauty experience. Our commitment to excellence 
+            ensures that every product we offer is carefully 
+            selected to enhance your natural radiance and empower 
+            yourself. <br><br>At Aquamarine, 
+            we invite you to explore, discover, and celebrate the
+            beauty within you.
+        </p>
+    </div>
+</div>
+
+
+<div class="section2-aboutus">
+    <div data-aos="fade-down" data-aos-duration="1000">
+        <h1>Our Store</h1>
+    </div>    
+        <div class="image-wrapper" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="400" data-aos-offset="0">
+            <img src="images/store0.jpg" alt="Large Image" class="large-image">
+            <div class="small-images" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="800" data-aos-offset="0">
+                <img src="images/store1.jpg" alt="Small Image 1" class="small-image">
+                <img src="images/store2.jpg" alt="Small Image 2" class="small-image">
+            </div>
+            <div class="small-images" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="1200" data-aos-offset="0">
+                <img src="images/store3.jpeg" alt="Small Image 1" class="small-image">
+                <img src="images/store4.jpg" alt="Small Image 2" class="small-image">
+            </div>
+        </div>
+</div>
+
+
+<div class="section3-aboutus">
+     <div data-aos="fade-down" data-aos-duration="1000">
+        <h2>World Wide Shipping</h2>
+        <p>
+            We pride ourselves on offering fast and secure international shipping, ensuring that the quality and innovation of our 
+            products reach customers worldwide.Following beauty trends has never been easier with our global shipping service. <br>
+            Join us today for an unforgettable journey into the world of beauty!  
+        </p>
+    </div>   
+    <div class="shipping-img" data-aos="zoom-in" data-aos-duration="1500">
+        <img src="images/shipping.png" alt="World Wide Shipping">
+    </div>
+</div>
+    
+<?php
+include "include/footer.php";
+?> 
